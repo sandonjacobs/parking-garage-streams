44a121c5d26d8950a1e1d7621d2193155e14441c
Sandon Jacobs <sjacobs@confluent.io>
2025-06-25 07:56:46 -0400

    moving app to datagen

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 // Include the `app`, `utils`, and `kafka-streams` subprojects in the build.
 // If there are changes in only one of the projects, Gradle will rebuild only the one that has changed.
 // Learn more about structuring projects with Gradle - https://docs.gradle.org/8.7/userguide/multi_project_builds.html
-include(":app")
+include(":datagen")
 include(":utils")
 include(":kafka-streams")
 
